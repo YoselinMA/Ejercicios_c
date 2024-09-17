1499270dea10c40930e22d78135284dd5eacabcd
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-09-16 23:07:53 -0600

    Create Eje1.c

---
+++ Eje1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+    double anios = 19;
+    double dias = 365;
+    double horas = 24;
+    double min = 60;
+    double seg = 60;
+     printf("%f", anios*dias*horas*min*seg);
+
+     printf("\n");
+     return 0;
+}
