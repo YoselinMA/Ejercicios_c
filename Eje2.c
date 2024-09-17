70dafc15689e2f409d406286766495b6552d81e5
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-09-16 23:08:49 -0600

    Create Eje2.c

---
+++ Eje2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int var1 = 10;
+    int var2 = 20;
+    int var3 = 2;
+    int res = 0;
+    
+    res = var1*var2*var3;
+    printf("El resultado es: %d", res);
+    printf("El resultado de multiplicar  %d * %d * %d es: %d", var1, var2,var3, res);
+
+    
+}
