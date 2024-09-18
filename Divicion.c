f7fcc32112027e0fd6a1ebd0bb7a419eb7c9b097
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-09-17 22:17:43 -0600

    Create Divicion.c

---
+++ Divicion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+    double num1 = 2;
+    double num2 = 38;
+    double res = 0;
+    res = num1/num2;
+    printf("El resultado de dividir  %f / %f  es: %d", num1, num2, res);
+
+
+}
