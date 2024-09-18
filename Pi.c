5cecbb1d892b4f61f2c088f4ba72525e9d02a008
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-09-17 22:17:10 -0600

    Create Pi.c

---
+++ Pi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+    double r = 5;
+    double pi = 3.1416;
+    double area = pi * r * r;
+
+    printf("%f", area);
+
+    printf("\n");
+    system("pause");
+    return 0;
+    
+}
