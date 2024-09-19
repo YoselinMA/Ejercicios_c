e0903e222bebce03fd740d1a5ea91adc07d09da1
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-09-18 21:25:06 -0600

    Create circulo.c

---
+++ circulo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+    double diametro = 10;
+    double pi = 3.1416;
+    double rad= 0;
+    double result = 0;
+
+    rad= diametro / 2;
+    result = rad * pi;
+     printf("El resultado del area del circulo es:  %f * %f  es: %f", rad, pi, result);
+
+
+
+}
